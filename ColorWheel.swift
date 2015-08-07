aa879281bd6e115643ee2b5f917b07a6f9f4e9b0
Lydia Khashina <adatheleader@gmail.com>
2015-08-07 14:57:40 +0300

    color palette updated

--- ColorWheel.swift
+++ ColorWheel.swift
@@ -12,13 +12,21 @@ import UIKit
 
 struct ColorWheel {
     let colorsArray = [
-        UIColor(red: 90/225.0, green: 187/255.0, blue: 181/225.0, alpha: 1.0), //teal color
-        UIColor(red: 222/225.0, green: 171/255.0, blue: 66/225.0, alpha: 1.0), //yellow color
-        UIColor(red: 223/225.0, green: 86/255.0, blue: 94/225.0, alpha: 1.0), //red color
-        UIColor(red: 239/225.0, green: 130/255.0, blue: 100/225.0, alpha: 1.0), //orange color
-        UIColor(red: 77/225.0, green: 75/255.0, blue: 82/225.0, alpha: 1.0), //dark color
-        UIColor(red: 105/225.0, green: 94/255.0, blue: 133/225.0, alpha: 1.0), //purple color
-        UIColor(red: 85/225.0, green: 176/255.0, blue: 112/225.0, alpha: 1.0), //green color
+        UIColor(red: 90/225.0, green: 187/255.0, blue: 181/225.0, alpha: 1.0),
+        UIColor(red: 222/225.0, green: 171/255.0, blue: 66/225.0, alpha: 1.0),
+        UIColor(red: 223/225.0, green: 86/255.0, blue: 94/225.0, alpha: 1.0),
+        UIColor(red: 239/225.0, green: 130/255.0, blue: 100/225.0, alpha: 1.0),
+        UIColor(red: 77/225.0, green: 75/255.0, blue: 82/225.0, alpha: 1.0),
+        UIColor(red: 105/225.0, green: 94/255.0, blue: 133/225.0, alpha: 1.0),
+        UIColor(red: 85/225.0, green: 176/255.0, blue: 112/225.0, alpha: 1.0),
+        UIColor(red: 68/225.0, green: 211/255.0, blue: 211/225.0, alpha: 1.0),
+        UIColor(red: 225/225.0, green: 160/255.0, blue: 82/225.0, alpha: 1.0),
+        UIColor(red: 255/225.0, green: 82/255.0, blue: 82/225.0, alpha: 1.0),
+        UIColor(red: 255/225.0, green: 125/255.0, blue: 125/225.0, alpha: 1.0),
+        UIColor(red: 73/225.0, green: 59/255.0, blue: 79/225.0, alpha: 1.0),
+        UIColor(red: 132/225.0, green: 94/255.0, blue: 127/225.0, alpha: 1.0),
+        UIColor(red: 75/225.0, green: 233/255.0, blue: 75/225.0, alpha: 1.0),
+        
     ]
     
     
